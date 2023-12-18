9f307c6dca349e6a48dc9521643c8040cdb61d57
Sarthak Shrivastava <72563740+SarthakShri@users.noreply.github.com>
2023-12-18 16:00:36 +0530

    Create Q1.py
    Partial

---
+++ Q1.py
@@ -0,0 +1,13 @@
+class Node:
+  def __init__(self,val):
+    self.val = val
+    self.left = None
+    self.right = None
+    
+def findIndex(arr,x):
+  if arr == []:
+    return "Not Present"
+  
+
+
+findIndex([4,1,2,3,5,6,7], 6) 
